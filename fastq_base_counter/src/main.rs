955ba88c136b614a929be380d77b2594bcd3f7ac
Johannes Alneberg <alneberg@kth.se>
2024-01-26 21:04:18 +0100

    Trying out a config struct

--- fastq_base_counter/src/main.rs
+++ fastq_base_counter/src/main.rs
@@ -2,8 +2,22 @@ use std::fs::File;
 use std::env;
 use std::io::{BufRead, BufReader, Result};
 
-fn read_file_line_by_line(filepath: &str) -> Result<()> {
-    let file = File::open(filepath)?;
+struct Config {
+    file_path: String,
+}
+
+fn parse_config(args: Vec<String>) -> Config {
+    if args.len() < 2 {
+        panic!("not enough parameters");
+    }
+
+    let file_path = args[1].clone();
+
+    Config { file_path }
+}
+
+fn read_file_line_by_line(file_path: &str) -> Result<()> {
+    let file = File::open(file_path)?;
     let reader = BufReader::new(file);
 
     for line in reader.lines() {
@@ -16,9 +30,11 @@ fn read_file_line_by_line(filepath: &str) -> Result<()> {
 fn main() -> Result<()> {
     let args: Vec<String> = env::args().collect();
 
-    let file_path: &str = &args[1];
-    if let Err(e) = read_file_line_by_line(file_path) {
+    let config: Config = parse_config(args);
+
+    if let Err(e) = read_file_line_by_line(&config.file_path) {
         eprintln!("Error reading file: {}", e);
+        return Err(e);
     }
 
     Ok(())
